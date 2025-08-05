b2a32a479b19753a2d77f2336d5c61dc6a90886a
HBVJH <142343978+HBVJH@users.noreply.github.com>
2025-08-05 21:08:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
-
 app = FastAPI()
-
 @app.get("/")
 def read_root():
     return {"message": "הקו מדבר!"}
-
 @app.post("/webhook")
 async def webhook(request: Request):
     return PlainTextResponse("שלום וברוך הבא לקו בינה, איך אפשר לעזור?")
