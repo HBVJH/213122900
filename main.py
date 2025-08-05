f5f6c8aaf9dc7d69454ced50af1847b15c7113dc
HBVJH <142343978+HBVJH@users.noreply.github.com>
2025-08-05 14:59:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "הקו מדבר!"}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    try:
+        data = await request.json()
+        user_text = data.get("text", "")
+    except:
+        user_text = ""
+
+    return {"text": "שלום וברוך הבא לקו בינה, איך אפשר לעזור?"}
