9094dd61cf918fb641fbae5af6aabd8e969a2c51
HBVJH <142343978+HBVJH@users.noreply.github.com>
2025-08-06 08:08:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
+
 app = FastAPI()
-@app.get("/")
-def root():
-    return {"message": "הקו מדבר!"}
+
 @app.api_route("/webhook", methods=["GET", "POST"])
 async def webhook(request: Request):
-    if request.method == "GET":
-return PlainTextResponse("say=שלום וברוך הבא לקו בינה, איך אפשר לעזור?")
-    elif request.method == "POST":
-        try:
-            data = await request.json()
-            user_text = data.get("text", "")
-        except:
-            user_text = ""
-return PlainTextResponse("say=שלום וברוך הבא לקו בינה, איך אפשר לעזור?")
+    return PlainTextResponse("say=שלום וברוך הבא לקו בינה, איך אפשר לעזור?")
